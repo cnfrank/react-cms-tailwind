08dee5ef1223cf5a88eb312d36ba6eda111b2cae
FrankZhou <9570075@qq.com>
2023-01-25 22:51:44 +1300

    Install Tailwind CSS&Configure

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/*
+ * @Author: Frank Zhou
+ * @Email: cnfrank527@gmail.com
+ * @Date: 2023-01-25 22:50:48
+ * @LastEditors: Frank Zhou
+ * @LastEditTime: 2023-01-25 22:51:18
+ * @Description: file content
+ * @FilePath: /react-cms-tailwind/tailwind.config.cjs
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
